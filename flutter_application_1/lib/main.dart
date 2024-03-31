cac01e5b5d7dec0e76077270c9e3b17b2784a258
Belaid sarah <118467926+belaid-sarah@users.noreply.github.com>
2024-03-31 17:33:20 +0100

    first commit

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'pages/home.dart';
+import 'pages/loading.dart';
+import 'pages/choose_location.dart';
+/*import 'package:wolrd_time/pages/home.dart';*/
 
 void main() => runApp(MaterialApp(
-    home: NinjaCard()
+  initialRoute: '/home',
+    routes: {
+      '/':(context) => Loading(),
+      '/home':(context) => Home(),
+      '/location':(context) => ChooseLocation(),
+    },
+
   
   ));
 
 
-class NinjaCard extends StatelessWidget{
+/*class NinjaCard extends StatefulWidget{
+
+  int ninjaLevel = 0;
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -17,7 +34,18 @@ class NinjaCard extends StatelessWidget{
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+        
       ),
+      floatingActionButton:FloatingActionButton(
+        onPressed: (){
+          setState(() {
+          
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+
+        ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
@@ -100,3 +128,65 @@ class NinjaCard extends StatelessWidget{
       
   }
 }
+
+class QuoteList extends StatefulWidget {
+
+  List<Quote> quotes =[
+    Quote(author:'ocsar' , text:'')
+  ];
+
+  Widget quoteTemplate(quote){
+    return Card(
+      margin:EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Column(
+        children:<Widget> [
+         Text(
+          quote.text,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Colors.grey[600],
+          ),
+         ) ,
+         SizedBox(height: 6.0),
+         Text(
+          quote.author,
+          style: TextStyle(
+            fontSize: 14.0,
+            color: Colors.grey[800]
+          )
+         ),
+         SizedBox(height: 8.0),
+         FlatButt
+        ],
+        ),
+       )
+  }
+ 
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class  _QuoteListState extends State<QuoteList> {
+
+  List<String> quotes = [
+    'I dont want to be a',
+    'here we are'
+    'love  is not love',
+
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title:Text('awsome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body:Column(
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        )
+    );
+  }
+}
+*/
